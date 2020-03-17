26ec2868ede5a76d85814881f433903ab85396c6
James Chapman <chapmanjamesg@gmail.com>
2020-03-17 09:54:42 -0500

    added form.py to ingredients and updated the __init__ to add the needed imports

--- FamilyCB/FamilyCBapp/views/__init__.py
+++ FamilyCB/FamilyCBapp/views/__init__.py
@@ -8,3 +8,8 @@ from .members.form import member_edit_form, member_form
 from .recipes.list import list_recipes
 from .recipes.details import recipe_details
 from .recipes.form import recipe_edit_form, recipe_form
+
+
+from .ingredients.list import list_ingredients
+from .ingredients.details import ingredient_details
+from .ingredients.form import ingredient_edit_form, ingredient_form
---
+++ FamilyCB/FamilyCBapp/views/ingredients/form.py
@@ -0,0 +1,54 @@
+import sqlite3
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from FamilyCBapp.models import Ingredient, model_factory
+from .details import get_member
+from .details import get_ingredient
+from ..connection import Connection
+
+def get_ingredients():
+    with sqlite3.connect(Connection.db_path) as conn:
+        conn.row_factory = model_factory(Ingredient)
+        db_cursor = conn.cursor()
+
+        db_cursor.execute("""
+        SELECT
+            i.id,
+            i.name,
+            i.quantity,
+            i.measurement,
+            r.ingredientId
+
+        FROM FamilyCBapp_ingredient i
+        LEFT JOIN FamilyCBapp_ingredient r
+        ON  i.id = r.ingredientId
+        WHERE i.id = ?
+        """)
+
+        return db_cursor.fetchall()
+
+
+@login_required
+def ingredient_form(request):
+    if request.method == 'GET':
+        ingredients = get_ingredient()
+        template = 'ingredients/ingredients_form.html'
+        context = {
+            'all_ingredients': ingredients
+        }
+
+        return render(request, template, context)
+
+@login_required
+def ingredient_edit_form(request, ingredientId):
+    if request.method == 'GET':
+        ingredient = get_ingredient(ingredientId)
+        ingredients = get_ingredients()
+        
+        template = 'ingredients/ingredients_form.html'
+        context = {
+            'ingredient': ingredient,
+            'all_ingredients': ingredients
+        }
+
+        return render(request, template, context)
